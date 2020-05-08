2a6b76cd821a4e839787bbe7f9799106aee7707d
Otávio Augusto Mota <otavioamota4@gmail.com>
2020-05-07 21:21:33 -0300

    Projeto criado

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   string sexo;
+   int cervejas, refrigerante, espetos;
+
+   cout << "sexo:"; cin >> sexo;
+   cout << "quantidade de cervejas:"; cin >> cervejas;
+   cout << "quantidade de refrigerantes:"; cin >> refrigerante;
+   cout << "quantidade de espetos:"; cin >> espetos;
+
+    return 0;
+}
